2a4c3a55b0186a78eef12d896bf8705c2c312423
Aditya <adityac0104@gmail.com>
2026-08-02 00:43:25 +0530

    New Question

---
+++ Stack-Queue/08TrappingRainwater.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//traversing from both sides and calculating the max height from left and right and then calculating the water trapped at each index
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int Lmax=0,Rmax=0,total=0;
+        int l = 0;
+        int r = n-1;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(Lmax>height[l]){
+                    total += Lmax - height[l];
+                }
+                else{
+                    Lmax = height[l];
+                }
+                l++;
+            }
+                else{
+                    if(Rmax>height[r]){
+                        total +=Rmax -height[r];
+                    }
+                    else{
+                        Rmax = height[r];
+                    }
+                    r--;
+                }
+        }
+        return total;
+    }
+};
